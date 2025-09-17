317f12ffc8b68f2b91346409553706ee092fb504
안홍선 <moon72907@gmail.com>
2025-09-17 14:46:28 +0900

    feat : CI/CD관련 설정 파일 추가  #18

--- backend/src/main/java/com/back/BackApplication.java
+++ backend/src/main/java/com/back/BackApplication.java
@@ -3,11 +3,13 @@ package com.back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 //@SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class BackApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackApplication.class, args);
--- infra/variables.tf
+++ infra/variables.tf
@@ -10,5 +10,5 @@ variable "prefix" {
 
 variable "app_1_domain" {
   description = "app_1 domain"
-  default     = "api.challenge.noredsun.com"
+  default     = "api.nutree.noredsun.com"
 }
