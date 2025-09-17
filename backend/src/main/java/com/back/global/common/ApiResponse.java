939960386e33d2f4435aa89d3483a851fdcb0bd0
EoN <jaegyun1011@gmail.com>
2025-09-17 10:32:38 +0900

    refactor: ApiResponse 수정

--- backend/src/main/java/com/back/global/common/ApiResponse.java
+++ backend/src/main/java/com/back/global/common/ApiResponse.java
@@ -1,33 +1,32 @@
 package com.back.global.common;
 
 import lombok.Builder;
-import lombok.Getter;
 
 //API 응답 표준화
 @Builder
-@Getter
-public class ApiResponse<T> {
-    private final boolean success;
-    private final String code;
-    private final String message;
-    private final T content;
-
-    private final static String SUCCESS_CODE = "200";
-
-    // 성공 응답 생성 메서드
-    public static <T> ApiResponse<T> success(T content) {
-        return ApiResponse.<T>builder()
-                .success(true)
-                .code(SUCCESS_CODE)
-                .message("Success")
-                .content(content)
-                .build();
+public record ApiResponse<T>(
+        boolean success,
+        String code,
+        String message,
+        T content
+) {
+    public ApiResponse(String code, String message) {
+        this(code, message, null);
     }
 
-    public static <T> ApiResponse<T> success(String message, T content) {
+    public ApiResponse(String code, String message, T content) {
+        this(code.startsWith("2"), code, message, content);
+    }
+
+    // 성공 응답 생성 메서드
+    public static <T> ApiResponse<T> success(String code, String message) {
+        return success(code, message, null);
+    }
+
+    public static <T> ApiResponse<T> success(String code, String message, T content) {
         return ApiResponse.<T>builder()
                 .success(true)
-                .code(SUCCESS_CODE)
+                .code(code)
                 .message(message)
                 .content(content)
                 .build();
@@ -35,22 +34,15 @@ public class ApiResponse<T> {
 
     // 실패 응답 생성 메서드
     public static <T> ApiResponse<T> fail(String code, String message) {
-        return ApiResponse.<T>builder()
-                .success(false)
-                .code(code)
-                .message(message)
-                .content(null)
-                .build();
+        return fail(code, message, null);
     }
-
 
     public static <T> ApiResponse<T> fail(String code, String message, T content) {
         return ApiResponse.<T>builder()
                 .success(false)
                 .code(code)
                 .message(message)
-                .content(content) // 여기서 Map, List 등 데이터 가능
+                .content(content)
                 .build();
     }
-
 }
