e60367e5a70349f27381e14960e8f21eb5a09c14
heigangbae <j56233145@gmail.com>
2025-09-22 15:10:09 +0900

    feat : 웹소켓 의존성 추가

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.implementation
+
 plugins {
     java
     id("org.springframework.boot") version "3.5.5"
@@ -46,6 +48,8 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
+    implementation("org.springframework.boot:spring-boot-starter-websocket")
 }
 
 tasks.withType<Test> {
