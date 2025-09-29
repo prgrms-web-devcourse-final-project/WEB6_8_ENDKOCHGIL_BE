17a4a4ba0d517b004c626d4dd0c0625ea29c220c
EoN <jaegyun1011@gmail.com>
2025-09-29 16:45:10 +0900

    fix: Google 인증 디버깅 추가

--- backend/src/main/java/com/back/BackApplication.java
+++ backend/src/main/java/com/back/BackApplication.java
@@ -1,12 +1,17 @@
 package com.back;
 
-import com.google.api.client.util.Value;
+import com.google.auth.oauth2.GoogleCredentials;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.io.FileInputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -22,7 +27,18 @@ public class BackApplication {
 
     @PostConstruct
     public void setGoogleCredentialsEnv() {
-        // Google SDK가 사용할 수 있도록 환경변수 세팅
-        System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", googleCredentialsPath);
+        Path path = Paths.get(googleCredentialsPath).toAbsolutePath();
+        System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", path.toString());
+        System.out.println("*** Google credentials path set to: " + path);
+
+        try (FileInputStream serviceAccountStream = new FileInputStream(path.toFile())) {
+            GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccountStream);
+
+            System.out.println("*** Google credentials loaded successfully!");
+            System.out.println("*** Credentials type: " + credentials.getClass().getSimpleName());
+        } catch (Exception e) {
+            System.err.println("*** Failed to load Google credentials: " + e.getMessage());
+        }
     }
+
 }
