eb7b1e8cc2710f4958bb89a9610d3901e05dd402
heigangbae <j56233145@gmail.com>
2025-09-29 16:18:34 +0900

    feat : 레벨 리포지터리 생성

---
+++ backend/src/main/java/com/back/domain/level/repository/LevelXPRepository.java
@@ -0,0 +1,7 @@
+package com.back.domain.level.repository;
+
+import com.back.domain.level.entity.LevelXP;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LevelXPRepository extends JpaRepository<LevelXP, Integer> {
+}
