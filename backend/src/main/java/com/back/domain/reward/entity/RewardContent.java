65636abee87989dd300a2409ccd341973691cd15
안홍선 <moon72907@gmail.com>
2025-09-30 10:14:56 +0900

    reward 보상하하는 서비스 추가

--- backend/src/main/java/com/back/domain/reward/entity/RewardContent.java
+++ backend/src/main/java/com/back/domain/reward/entity/RewardContent.java
@@ -1,8 +1,12 @@
 package com.back.domain.reward.entity;
 
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.Setter;
 
 @Embeddable
+@Getter
+@Setter
 public class RewardContent {
     ContentType contentType;
     int value;
--- backend/src/main/java/com/back/domain/reward/service/RewardService.java
+++ backend/src/main/java/com/back/domain/reward/service/RewardService.java
@@ -1,5 +1,8 @@
 package com.back.domain.reward.service;
 
+import com.back.domain.member.entity.Member;
+import com.back.domain.member.service.MemberService;
+import com.back.domain.reward.entity.ContentType;
 import com.back.domain.reward.entity.Reward;
 import com.back.domain.reward.entity.RewardContent;
 import com.back.domain.reward.entity.RewardType;
@@ -13,15 +16,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RewardService {
     private  final RewardRepository rewardRepository;
-
+    private final MemberService  memberService;
     public void crearteReward (RewardType rewardType, List<RewardContent> rewardContents, int requiredValue )
     {
         rewardRepository.save(new Reward(rewardType,rewardContents,requiredValue));
     }
-    public void findReward()
-    {
+
+    public void fineReward() {
+
         rewardRepository.findAll();
     }
+
     public void updateReward(int id, RewardType rewardType, List<RewardContent> rewardContents, int requiredValue)
     {
         Reward reward = rewardRepository.findById(id).get();
@@ -34,4 +39,34 @@ public class RewardService {
     {
         rewardRepository.deleteById(id);
     }
+
+    public void giveReward(int memberId, int value, int rewardId)
+    {
+        Reward reward = rewardRepository.findById(rewardId).get();
+        Member member = memberService.findById(memberId).get();
+        if(value >= reward.getRequireValue())
+        {
+            for (RewardContent rewardContent : reward.getRewards())
+            {
+                if(rewardContent.getContentType() == ContentType.XP)
+                {
+                    memberService.modifyStatus(member,0,0,0);
+                }
+                else if(rewardContent.getContentType() == ContentType.MONEY)
+                {
+
+                    memberService.modifyStatus(member,member.getLevel(),member.getXp(),member.getMoney()+rewardContent.getValue());
+                }
+                else if(rewardContent.getContentType() == ContentType.ITEM)
+                {
+                    memberService.modifyItem(member,rewardContent.getValue());
+                }
+                else if(rewardContent.getContentType() == ContentType.TITLE)
+                {
+                    memberService.modifyTitle(member,rewardContent.getValue());
+                }
+            }
+        }
+    }
+
 }
