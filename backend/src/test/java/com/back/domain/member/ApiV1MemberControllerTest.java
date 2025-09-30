6d003cb315619e89f6522399f96ea2cad74b46b7
EoN <jaegyun1011@gmail.com>
2025-09-30 16:30:01 +0900

    feat: MemberTest 테스트케이스 추가

--- backend/src/test/java/com/back/domain/member/ApiV1MemberControllerTest.java
+++ backend/src/test/java/com/back/domain/member/ApiV1MemberControllerTest.java
@@ -1,8 +1,14 @@
 package com.back.domain.member;
 
+import com.back.domain.item.entity.Item;
+import com.back.domain.item.entity.ItemType;
+import com.back.domain.item.repository.ItemRepository;
 import com.back.domain.member.controller.ApiV1MemberController;
 import com.back.domain.member.entity.Member;
 import com.back.domain.member.service.MemberService;
+import com.back.domain.title.entity.Title;
+import com.back.domain.title.repository.TitleRepository;
+import com.jayway.jsonpath.JsonPath;
 import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -21,6 +27,8 @@ import java.time.format.DateTimeFormatter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -34,21 +42,41 @@ public class ApiV1MemberControllerTest {
     private final String baseUrl = "/api/v1/members";
 
     @Autowired
-    private MemberService memberService;
-    @Autowired
     private MockMvc mvc;
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private MemberService memberService;
+    @Autowired
+    private ItemRepository itemRepository;
+    @Autowired
+    private TitleRepository titleRepository;
+
     private Member user1;
+    private Item item1, item2;
+    private Title title1, title2;
 
     @BeforeEach
     void setUp() {
+        item1 = itemRepository.save(new Item(ItemType.AVATAR, "아이템1", ""));
+        item2 = itemRepository.save(new Item(ItemType.AVATAR, "아이템2", ""));
+        title1 = titleRepository.save(new Title("칭호1"));
+        title2 = titleRepository.save(new Title("칭호2"));
+
         user1 = memberService.signup(
                 "test1@test.com",
                 "test123",
                 "테스트유저1"
         );
+
+        user1.addItem(item1);
+        user1.addItem(item2);
+        user1.setItem(item1);
+
+        user1.addTitle(title1);
+        user1.addTitle(title2);
+        user1.setTitle(title1);
     }
 
     @Test
@@ -69,6 +97,7 @@ public class ApiV1MemberControllerTest {
                 .andDo(print());
 
         Member member = memberService.findByEmail("newuser@test.com").get();
+        assertNotNull(member);
 
         resultActions
                 .andExpect(handler().handlerType(ApiV1MemberController.class))
@@ -77,7 +106,7 @@ public class ApiV1MemberControllerTest {
                 .andExpect(jsonPath("$.code").value("201"))
                 .andExpect(jsonPath("$.message").value("[Member] Success: 회원 가입"))
                 .andExpect(jsonPath("$.content").exists())
-                .andExpect(jsonPath("$.content.name").value(member.getName()));
+                .andExpect(jsonPath("$.content.id").value(member.getId()));
     }
 
     @Test
@@ -104,18 +133,20 @@ public class ApiV1MemberControllerTest {
                 .andExpect(jsonPath("$.message").value("[Member] Success: 로그인"))
                 .andExpect(jsonPath("$.content").exists())
                 .andExpect(jsonPath("$.content.item").exists())
-                .andExpect(jsonPath("$.content.item.name").value(user1.getName()))
+                .andExpect(jsonPath("$.content.item.id").value(user1.getId()))
                 .andExpect(jsonPath("$.content.apiKey").value(user1.getApiKey()))
                 .andExpect(jsonPath("$.content.accessToken").isNotEmpty());
 
         resultActions.andExpect(
                 result -> {
                     Cookie apiKeyCookie = result.getResponse().getCookie("apiKey");
+                    assertNotNull(apiKeyCookie);
                     assertThat(apiKeyCookie.getValue()).isEqualTo(user1.getApiKey());
                     assertThat(apiKeyCookie.getPath()).isEqualTo("/");
                     assertThat(apiKeyCookie.getAttribute("HttpOnly")).isEqualTo("true");
 
                     Cookie accessTokenCookie = result.getResponse().getCookie("accessToken");
+                    assertNotNull(accessTokenCookie);
                     assertThat(accessTokenCookie.getValue()).isNotBlank();
                     assertThat(accessTokenCookie.getPath()).isEqualTo("/");
                     assertThat(accessTokenCookie.getAttribute("HttpOnly")).isEqualTo("true");
@@ -206,32 +237,6 @@ public class ApiV1MemberControllerTest {
     }
 
     @Test
-    @DisplayName("닉네임 변경")
-    void modifyName() throws Exception {
-        ResultActions resultActions = mvc
-                .perform(
-                        put(baseUrl + "/modify/name")
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .content("""
-                                        {
-                                            "name": "유저1변경"
-                                        }
-                                        """.stripIndent())
-                                .cookie(new Cookie("apiKey", user1.getApiKey()))
-                )
-                .andDo(print());
-
-        resultActions
-                .andExpect(handler().handlerType(ApiV1MemberController.class))
-                .andExpect(handler().methodName("modifyName"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.code").value("200"))
-                .andExpect(jsonPath("$.message").value("[Member] Success: 닉네임 변경"))
-                .andExpect(jsonPath("$.content").exists())
-                .andExpect(jsonPath("$.content.name").value(user1.getName()));
-    }
-
-    @Test
     @DisplayName("회원 정보 수정")
     void modifyProfile() throws Exception {
         ResultActions resultActions = mvc
@@ -240,6 +245,7 @@ public class ApiV1MemberControllerTest {
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content("""
                                         {
+                                            "name": "유저1수정",
                                             "birth": "2050-10-20",
                                             "gender": "FEMALE"
                                         }
@@ -307,7 +313,95 @@ public class ApiV1MemberControllerTest {
                 .andExpect(jsonPath("$.code").value("200"))
                 .andExpect(jsonPath("$.message").value("[Member] Success: 사용자 정보 확인 (%s)".formatted(user1.getEmail())))
                 .andExpect(jsonPath("$.content").exists())
-                .andExpect(jsonPath("$.content.name").value(user1.getName()));
+                .andExpect(jsonPath("$.content.id").value(user1.getId()));
+    }
+
+    @Test
+    @DisplayName("보유한 아이템 정보 확인")
+    void myItems() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(
+                        get(baseUrl + "/me/items")
+                                .cookie(new Cookie("apiKey", user1.getApiKey()))
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1MemberController.class))
+                .andExpect(handler().methodName("myItems"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.message").value("[Member] Success: 보유한 아이템 확인"))
+                .andExpect(jsonPath("$.content").exists())
+                .andExpect(result -> {
+                    String json = result.getResponse().getContentAsString();
+                    int count = 0;
+                    for (ItemType type : ItemType.values()) {
+                        int size = JsonPath.read(json, "$.content.items." + type.name() + ".length()");
+                        count += size;
+                    }
+                    assertEquals(user1.getOwnedItems().size(), count);
+                });
+    }
+
+    @Test
+    @DisplayName("보유한 칭호 정보 확인")
+    void myTitles() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(
+                        get(baseUrl + "/me/titles")
+                                .cookie(new Cookie("apiKey", user1.getApiKey()))
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1MemberController.class))
+                .andExpect(handler().methodName("myTitles"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.message").value("[Member] Success: 보유한 칭호 확인"))
+                .andExpect(jsonPath("$.content").exists())
+                .andExpect(jsonPath("$.content.titles.length()").value(user1.getOwnedTitles().size()));
+    }
+
+    @Test
+    @DisplayName("아이템 장착")
+    void equipItem() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(
+                        put(baseUrl + "/equip/item/" + item2.getId())
+                                .cookie(new Cookie("apiKey", user1.getApiKey()))
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1MemberController.class))
+                .andExpect(handler().methodName("equipItem"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.message").value("[Member] Success: 아이템 장착"))
+                .andExpect(jsonPath("$.content").exists())
+                .andExpect(jsonPath("$.content.items." + item2.getType()).value(item2.getId()));
+    }
+
+    @Test
+    @DisplayName("칭호 장착")
+    void equipTitle() throws Exception {
+        ResultActions resultActions = mvc
+                .perform(
+                        put(baseUrl + "/equip/title/" + title2.getId())
+                                .cookie(new Cookie("apiKey", user1.getApiKey()))
+                )
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(ApiV1MemberController.class))
+                .andExpect(handler().methodName("equipTitle"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.message").value("[Member] Success: 칭호 장착"))
+                .andExpect(jsonPath("$.content").exists())
+                .andExpect(jsonPath("$.content.title").value(title2.getId()));
     }
 
     @Test
