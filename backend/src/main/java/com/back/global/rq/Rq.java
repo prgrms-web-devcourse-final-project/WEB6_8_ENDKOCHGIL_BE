6b3ac114df04df6267056d5ff2d84a01b9368023
EoN <jaegyun1011@gmail.com>
2025-10-02 09:22:12 +0900

    fix: SameSite 설정 수정

--- backend/src/main/java/com/back/global/rq/Rq.java
+++ backend/src/main/java/com/back/global/rq/Rq.java
@@ -83,7 +83,7 @@ public class Rq {
         cookie.setHttpOnly(true);
         cookie.setDomain("localhost");
         cookie.setSecure(true);
-        cookie.setAttribute("SameSite", "Strict");
+        cookie.setAttribute("SameSite", "None");
 
         if (value.isBlank()) cookie.setMaxAge(0);
         else cookie.setMaxAge(60 * 60 * 24 * 365);
